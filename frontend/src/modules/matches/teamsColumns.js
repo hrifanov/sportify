0c986487fceb6dc369eb5c040f0b0e395ef85f15
Jan Vetrovsky <vetrovsky.jan@gmail.com>
2022-11-09 17:20:00 +0100

    drag and drop finished

---
+++ frontend/src/modules/matches/teamsColumns.js
@@ -0,0 +1,19 @@
+import { players } from '../clubs/players';
+
+export const teamsColumns = [
+  {
+    id: 1,
+    name: 'Team 1',
+    items: [],
+  },
+  {
+    id: 2,
+    name: 'Players',
+    items: players,
+  },
+  {
+    id: 3,
+    name: 'Team 2',
+    items: [],
+  },
+];
--- frontend/src/modules/matches/templates/CreateMatchTemplate.js
+++ frontend/src/modules/matches/templates/CreateMatchTemplate.js
@@ -1,5 +1,5 @@
 import AppHeader from 'src/shared/core/organisms/AppHeader';
-import { Box, Container, Flex, Text, Spinner, Stack } from '@chakra-ui/react';
+import { Box, Container, Flex, Text, Spinner, Stack, Heading } from '@chakra-ui/react';
 import { RouterLink } from 'src/shared/navigation';
 import { FiSettings } from 'react-icons/fi';
 import { players } from 'src/modules/clubs/players';
@@ -10,13 +10,48 @@ import {
 } from 'src/shared/design-system/icons';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { ProvidedRequiredArgumentsRule } from 'graphql';
+import { useState } from 'react';
+import { teamsColumns } from '../teamsColumns';
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
+  }
+};
 
 export default function CreateMatchTemplate({ club, loading }) {
-  const onDragEnd = (result) => {
-    if (!result.destination) {
-      return;
-    }
-  };
+  const [columns, setColumns] = useState(teamsColumns);
   return (
     <Flex direction="column" h={{ md: '100vh' }}>
       <AppHeader title="Club detail" />
@@ -28,29 +63,11 @@ export default function CreateMatchTemplate({ club, loading }) {
         )}
         {club && (
           <Stack spacing={5} direction="row">
-            <DragDropContext onDragEnd={onDragEnd}>
-              <Box w="full" h="full" bg="brand.boxBackground" borderRadius="base" py={4} px={5}>
-                <Droppable droppableId="team1">
-                  {(provided) => (
-                    <Flex
-                      direction="column"
-                      w="full"
-                      h="full"
-                      bg="brand.boxBackground"
-                      borderRadius="base"
-                      py={4}
-                      px={5}
-                      {...provided.droppableProps}
-                      ref={provided.innerRef}
-                    >
-                      {provided.placeholder}
-                    </Flex>
-                  )}
-                </Droppable>
-              </Box>
-              <Droppable droppableId="players">
-                {(provided) => (
+            <DragDropContext onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
+              {Object.entries(columns).map(([columnId, column], index) => {
+                return (
                   <Flex
+                    key={column.id}
                     direction="column"
                     w="full"
                     h="full"
@@ -58,45 +75,51 @@ export default function CreateMatchTemplate({ club, loading }) {
                     borderRadius="base"
                     py={4}
                     px={5}
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
                   >
-                    {[...players, ...players].map((player, i) => (
-                      <Draggable key={i} draggableId={'' + i} index={i}>
-                        {(provided) => (
-                          <div
+                    <Heading size="md" align="middle" mb={4}>
+                      {column.name}
+                    </Heading>
+                    <Droppable droppableId={columnId} key={columnId}>
+                      {(provided, snapshot) => {
+                        return (
+                          <Box
+                            {...provided.droppableProps}
                             ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
+                            bg={snapshot.isDraggingOver ? 'brand.secondary' : 'brand.boxBackground'}
                           >
-                            <p>{player.name}</p>
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
+                            {column.items.map((item, index) => {
+                              return (
+                                <Draggable key={item.id} draggableId={'' + item.id} index={index}>
+                                  {(provided, snapshot) => {
+                                    return (
+                                      <Box
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        bg={
+                                          snapshot.isDragging ? 'brand.dark' : 'brand.boxBackground'
+                                        }
+                                        px={2}
+                                        py={2}
+                                        _hover={{
+                                          background: 'brand.dark',
+                                        }}
+                                      >
+                                        {item.name}
+                                      </Box>
+                                    );
+                                  }}
+                                </Draggable>
+                              );
+                            })}
+                            {provided.placeholder}
+                          </Box>
+                        );
+                      }}
+                    </Droppable>
                   </Flex>
-                )}
-              </Droppable>
-              <Box w="full" h="full" bg="brand.boxBackground" borderRadius="base" py={4} px={5}>
-                <Droppable droppableId="team2">
-                  {(provided) => (
-                    <Flex
-                      direction="column"
-                      w="full"
-                      h="full"
-                      bg="brand.boxBackground"
-                      borderRadius="base"
-                      py={4}
-                      px={5}
-                      {...provided.droppableProps}
-                      ref={provided.innerRef}
-                    >
-                      {provided.placeholder}
-                    </Flex>
-                  )}
-                </Droppable>
-              </Box>
+                );
+              })}
             </DragDropContext>
           </Stack>
         )}
