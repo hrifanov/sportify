7a9e12b324b16ccffa276d6743cb891fad65299a
hrifanov <hrifanov@gmail.com>
2022-11-21 15:36:40 +0100

    [frontend] tabs

--- frontend/src/modules/clubs/apollo/queries.js
+++ frontend/src/modules/clubs/apollo/queries.js
@@ -10,3 +10,20 @@ export const FETCH_CLUBS = gql`
     }
   }
 `;
+
+export const FETCH_MATCHES = gql`
+  query {
+    matches(clubId: "636ecd9840d0be5c9a93e4f2") {
+      date
+      teams {
+        home {
+          name
+        }
+        guest {
+          name
+        }
+      }
+    }
+  }
+`;
+console.log(FETCH_CLUBS);
--- frontend/src/modules/clubs/templates/ClubDetailTemplate.js
+++ frontend/src/modules/clubs/templates/ClubDetailTemplate.js
@@ -11,42 +11,20 @@ import {
   Tr,
   Icon,
   Tbody,
-  Thead,
-  Th,
-  Spacer,
-  Button,
-  Popover,
-  PopoverTrigger,
-  PopoverBody,
-  PopoverContent,
   Spinner,
-  Show,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tab,
 } from '@chakra-ui/react';
 import { RouterLink } from 'src/shared/navigation';
+import StatisticsComp from '../molecules/StatisticsComp';
+import MatchesComp from '../molecules/MatchesComp';
 import { FiSettings } from 'react-icons/fi';
-import { players } from 'src/modules/clubs/players';
-import {
-  RoleAttackerIcon,
-  RoleDefenderIcon,
-  RoleGoalKeeperIcon,
-} from 'src/shared/design-system/icons';
-import { BsFilter } from 'react-icons/bs';
+import RequestsComp from '../molecules/RequestsComp';
 
-export default function ClubDetailTemplate({ club, loading }) {
-  const RoleIcon = ({ role }) => {
-    const getProps = ({ width }) => ({
-      width,
-      style: { margin: 'auto', color: '#9FB2D1' },
-    });
-    const Element = {
-      attacker: () => <RoleAttackerIcon {...getProps({ width: 15 })} />,
-      defender: () => <RoleDefenderIcon {...getProps({ width: 20 })} />,
-      goalkeeper: () => <RoleGoalKeeperIcon {...getProps({ width: 25 })} />,
-    }[role];
-
-    return <Element />;
-  };
-
+export default function ClubDetailTemplate({ club, loading, matches }) {
   return (
     <Flex direction="column" h={{ md: '100vh' }}>
       <AppHeader title="Club detail" />
@@ -58,7 +36,7 @@ export default function ClubDetailTemplate({ club, loading }) {
         )}
         {club && (
           <Flex gap={6} h="full" direction={['column', null, 'row']}>
-            <Flex direction="column" w={{ md: 5 / 12 }} gap={4}>
+            <Flex direction="column" w={{ md: '20%' }} gap={4}>
               <Box w="full" bg="brand.boxBackground" borderRadius="base" pt={4} pb={6} px={5}>
                 <Text fontWeight="bold" color="brand.title" fontSize="xl">
                   Information
@@ -106,148 +84,43 @@ export default function ClubDetailTemplate({ club, loading }) {
                   Manage team
                 </RouterLink>
               </Box>
-              <Show above="md">
-                <Box w="full" h="full" bg="brand.boxBackground" borderRadius="base" py={4} px={5}>
-                  <Text fontWeight="bold" color="brand.title" fontSize="xl">
-                    Join requests
-                  </Text>
-                </Box>
-              </Show>
+              <RequestsComp></RequestsComp>
             </Flex>
-
+            <Tabs display={{ md: 'none' }} isFitted>
+              <TabList>
+                <Tab>Players</Tab>
+                <Tab>Matches</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <StatisticsComp></StatisticsComp>
+                </TabPanel>
+                <TabPanel>
+                  <MatchesComp></MatchesComp>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
             <Flex
               direction="column"
-              w="full"
+              w={{ md: '50%' }}
               minW={0}
               h="full"
               bg="brand.boxBackground"
               borderRadius="base"
               py={4}
+              display={{ base: 'none', md: 'flex' }}
             >
-              <Flex
-                px={5}
-                gap={[2, null, 5]}
-                alignItems={{ md: 'center' }}
-                direction={['column', null, 'row']}
-              >
-                <Text fontWeight="bold" color="brand.title" fontSize="xl">
-                  Statistics
-                </Text>
-
-                <Spacer />
-
-                <Popover>
-                  <PopoverTrigger>
-                    <Button
-                      variant="light"
-                      as={Flex}
-                      alignItems="center"
-                      w="full"
-                      maxW={{ md: 40 }}
-                      m={0}
-                      cursor="pointer"
-                      gap={2}
-                    >
-                      <Text textTransform="capitalize">All players</Text>
-                      <Spacer />
-                      <Icon as={BsFilter} boxSize={6} />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent maxW={{ md: 40 }} bg="#3E4A66" border={0}>
-                    <PopoverBody as={Flex} gap={2} direction="column">
-                      <Button variant="popup" size="sm">
-                        All players
-                      </Button>
-                      <Button variant="popup" size="sm">
-                        Outfield
-                      </Button>
-                      <Button variant="popup" size="sm">
-                        Goalkeepers
-                      </Button>
-                    </PopoverBody>
-                  </PopoverContent>
-                </Popover>
-
-                <Popover>
-                  <PopoverTrigger>
-                    <Button
-                      maxW={{ md: 52 }}
-                      variant="light"
-                      as={Flex}
-                      alignItems="center"
-                      m={0}
-                      cursor="pointer"
-                      gap={2}
-                    >
-                      <Text textTransform="capitalize">Season 01/21 - 12/21</Text>
-                      <Spacer />
-                      <Icon as={BsFilter} boxSize={6} />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent maxW={{ md: 52 }} bg="#3E4A66" border={0}>
-                    <PopoverBody as={Flex} gap={2} direction="column">
-                      <Button variant="popup" size="sm">
-                        All time
-                      </Button>
-                      <Button variant="popup" size="sm">
-                        Season 01/22 - 12/22
-                      </Button>
-                      <Button variant="popup" size="sm">
-                        Season 01/21 - 12/21
-                      </Button>
-                    </PopoverBody>
-                  </PopoverContent>
-                </Popover>
-              </Flex>
-              <TableContainer
-                mt={2}
-                px={2}
-                overflowY="scroll"
-                style={{
-                  td: {
-                    textAlign: 'center',
-                  },
-                }}
-              >
-                <Table size={['sm', null, 'md']} variant="base" className="statistics-table">
-                  <Thead>
-                    <Tr>
-                      <Th textAlign="left">ID</Th>
-                      <Th>Role</Th>
-                      <Th textAlign="left">Name</Th>
-                      <Th fontWeight="bold">TP</Th>
-                      <Th>M</Th>
-                      <Th>Avg</Th>
-                      <Th>G</Th>
-                      <Th>A</Th>
-                      <Th>P</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody color="brand.secondary">
-                    {[...players, ...players].map((player, i) => (
-                      <Tr key={i}>
-                        <Td textAlign="left" textColor="white">
-                          {i + 1}.
-                        </Td>
-                        <Td textColor="white">
-                          <RoleIcon role={player.role} />
-                        </Td>
-                        <Td textAlign="left" textColor="white">
-                          {player.name}
-                        </Td>
-                        <Td textColor="white" fontWeight="bold">
-                          {player.totalPoints}
-                        </Td>
-                        <Td>{player.matches}</Td>
-                        <Td>{player.average}</Td>
-                        <Td>{player.goals}</Td>
-                        <Td>{player.assist}</Td>
-                        <Td>{player.penalty}</Td>
-                      </Tr>
-                    ))}
-                  </Tbody>
-                </Table>
-              </TableContainer>
+              <StatisticsComp></StatisticsComp>
+            </Flex>
+            <Flex
+              direction="column"
+              borderRadius="base"
+              w={{ md: '30%' }}
+              gap={4}
+              display={{ Base: 'none', md: 'flex' }}
+              bg="brand.boxBackground"
+            >
+              <MatchesComp></MatchesComp>
             </Flex>
           </Flex>
         )}
